f9e74091a7c72a87c145375fb195d6c8c8a8f3d6
Hzen <mengma023@gmail.com>
2023-12-09 10:10:46 +0800

    Revert "ipq807x: drop all xiaomi devices support"
    This reverts commit edbd8d2e9839357f3a4f0a06174d243f362b1544.

--- target/linux/ipq807x/image/generic.mk
+++ target/linux/ipq807x/image/generic.mk
@@ -36,7 +36,7 @@ define Device/redmi_ax6
 	$(call Device/xiaomi_ax3600)
 	DEVICE_VENDOR := Redmi
 	DEVICE_MODEL := AX6
-	DEVICE_PACKAGES := ipq-wifi-redmi_ax6
+	DEVICE_PACKAGES := ipq-wifi-redmi_ax6 uboot-envtools
 endef
 TARGET_DEVICES += redmi_ax6
 
@@ -62,6 +62,7 @@ define Device/xiaomi_ax3600
 	PAGESIZE := 2048
 	DEVICE_DTS_CONFIG := config@ac04
 	SOC := ipq8071
+<<<<<<< HEAD
 	DEVICE_PACKAGES := ipq-wifi-xiaomi_ax3600 kmod-ath10k-ct-smallbuffers ath10k-firmware-qca9887-ct
 endef
 TARGET_DEVICES += xiaomi_ax3600
@@ -80,6 +81,13 @@ define Device/xiaomi_ax9000
 endef
 TARGET_DEVICES += xiaomi_ax9000
 
+=======
+	DEVICE_PACKAGES := ath10k-firmware-qca9887-ct ipq-wifi-xiaomi_ax3600 \
+	kmod-ath10k-ct uboot-envtools
+endef
+TARGET_DEVICES += xiaomi_ax3600
+
+>>>>>>> parent of edbd8d2e9 (ipq807x: drop all xiaomi devices support)
 define Device/zte_mf269
 	$(call Device/FitImage)
 	$(call Device/UbiFit)
